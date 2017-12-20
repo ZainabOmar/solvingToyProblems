4679cdab74b9b869a94cbaf5ec51a62a487b5d35
zainab <zainab.hammami.zh@gmail.com>
2017-12-20 11:55:04 +0200

    gap in primes solved

--- gapInPrimes.js
+++ gapInPrimes.js
@@ -38,12 +38,57 @@
 //is not a 6-gap because there is 103in between and 103-109
 //is not a 6-gap because there is 107in between.
 
-function gapInPrime (g, m, n) {
-	var counter = 0;
-	for (var i = m; i<n; i++) {
-		
+function gap(g, m, n) {
+	var arr = [];
+	var result = [];
+	
+	//we need a function to check for all the prime numbers.
+	//(will be used later in for loop) 
+	var isPrime = function (n) {
+		for (var i = 2; i < n; i++) {
+			if (n%i === 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+// find out the prime numbers and save them in an array.
+for (var i = m; i<=n; i++) {
+	if (isPrime(i)) {
+		arr.push(i);
 	}
 }
+
+// saving the adjacent prime numbers in to groups.
+for (var t = 0; t < arr.length-1; t++) {
+	var temp = arr[t];
+	arr[t] = [temp, arr[t+1]]
+}
+
+// to handle the array with groups only we pop the last element from the array.
+arr.pop();
+
+// check the gap with the difference between the numbers in each group.
+for (var j = 0; j < arr.length; j++) {
+	if (g === arr[j][1]-arr[j][0]) {
+		result.push(arr[j][0], arr[j][1])
+	}
+}
+
+// if there were no match then there is no result.
+if(result.length === 0){
+	return null;
+}
+
+//otherwisw there should be a result,
+//if there were more than one match then just bring the first one.
+if (result.length > 2) {
+	result.splice(2)
+}
+return result;
+}
+
 
 
 
