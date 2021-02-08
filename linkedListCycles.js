e9c76c9b1625cfa621bd442bcc66997a866c6c6d
ZainabOmar <zainab.hammami.zh@gmail.com>
2021-02-08 21:40:18 +0200

    editing question

--- linkedListCycles.js
+++ linkedListCycles.js
@@ -1,14 +1,18 @@
 /*
- * Assignment: Write a function that returns true if a linked list contains a cycle, or false if it terminates somewhere
+ * Assignment: Write a function that returns true if a linked list 
+ * contains a cycle, or false if it terminates somewhere
  *
  * Explanation:
  * 
- * Generally, we assume that a linked list will terminate in a null next pointer, as follows:
+ * Generally, we assume that a linked list will terminate 
+ in a null next pointer, as follows:
  *
  * A -> B -> C -> D -> E -> null
  *
- * A 'cycle' in a linked list is when traversing the list would result in visiting the same nodes over and over
- * This is caused by pointing a node in the list to another node that already appeared earlier in the list. Example:
+ * A 'cycle' in a linked list is when traversing the list would
+ * result in visiting the same nodes over and over
+ * This is caused by pointing a node in the list to another node 
+ * that already appeared earlier in the list. Example:
  *
  * A -> B -> C
  *      ^    |
